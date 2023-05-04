c052d2da41bef7525ac48f5295ce9d66d3e7483e
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-04 06:15:18 -0400

    data fetched

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { ProductCard } from "./ProductCard";
+
+
 
 
  export const Products = () => {
+const [items,setItems]=useState([]);
+
+useEffect(()=>{
+	fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(data=> setItems(data))
+},[])
+
+
 	return (
 		<div>
-			hello world
+			<p>Total Product {items.length}</p>
+			{items.map(product =>  )}
 		</div>
 	);
 }
